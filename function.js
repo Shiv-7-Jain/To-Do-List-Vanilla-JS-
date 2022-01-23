c66a90e7770fd3ee54aac288b10c34dcb014f232
Shivam Jain <shiv7jain@gmail.com>
2022-01-23 14:11:02 +0530

    Added Animations using Animate.css

--- function.js
+++ function.js
@@ -8,7 +8,7 @@ var addTask = document.getElementById('addTask');
 
 var array_tasks = ['Complete Front End Projects','Start React'];
 
-function checkedNewTask() {
+function checkNewTask() {
     // Looping thourgh each checked buttons and setting up event listeners
         let count = 0;
         let id_string = 'checkbox' + (array_tasks.length-1);
@@ -17,9 +17,19 @@ function checkedNewTask() {
             if(count % 2 == 0){
                 // Changing the icon is the user clicks on the checkbox
                 button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','checkmark-done-circle-outline');
+                // Handling Animations
+                button_id.parentElement.classList.remove('animate__animated');
+                button_id.parentElement.classList.remove('animate__headShake');
+                button_id.parentElement.classList.add('animate__animated');
+                button_id.parentElement.classList.add('animate__flash');
             }else{
                 // setting the previous icon if the user clicks on the ion again.
                 button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','ellipse-outline');
+                // Handling Animations
+                button_id.parentElement.classList.remove('animate__animated');
+                button_id.parentElement.classList.remove('animate__flash');
+                button_id.parentElement.classList.add('animate__animated');
+                button_id.parentElement.classList.add('animate__headShake');
             }
             // Toggling the class which makes the text decoration line-through whenever checkbox is checked
             button_id.parentElement.classList.toggle('design');
@@ -28,11 +38,14 @@ function checkedNewTask() {
 }
 
 function deleteNewTask() {
-    // Looping thourgh each checked buttons and setting up event listeners
+    // Looping thourgh each checked buttons and setting up event listener
         let id_string = 'delete' + (array_tasks.length-1);
         let button_id = document.getElementById(id_string);
         button_id.addEventListener('click',function(){
+            button_id.parentElement.classList.add('animate__backOutDown');
+        setTimeout(() => {
             button_id.parentElement.remove();
+        }, 500);
         });
 }
 // Adding a new Task to the list
@@ -43,7 +56,7 @@ function add_new_task(){
     new_task.value = '';
     // Function to display the newest task to the screen
     displayNewTasks();
-    checkedNewTask();
+    checkNewTask();
     deleteNewTask();
 }
 
@@ -52,56 +65,54 @@ addTask.addEventListener('click',add_new_task);
 
 
 // Display The Tasks 
-function displayTasks(){
-    for(let i = 0;i < array_tasks.length;i++){
+for(let i = 0;i < array_tasks.length;i++){
 
-        // Making a Task
-        const new_task = document.createElement('div');
-        new_task.classList.add('task');
+    // Making a Task
+    const new_task = document.createElement('div');
+    new_task.classList.add('task');
 
-        // Making the content div
-        const content = document.createElement('div');
-        content.classList.add('content');
+    // Making the content div
+    const content = document.createElement('div');
+    content.classList.add('content');
 
-        // Creating the checkbox icon and placing it inside a button
-        const icon = document.createElement('ion-icon');
-        icon.setAttribute('name','ellipse-outline');
-        
-        const check_button = document.createElement('button');
-        check_button.appendChild(icon);
-        // Setting the id of the checkbox
-        check_button.setAttribute('id','checkbox' + i);
+    // Creating the checkbox icon and placing it inside a button
+    const icon = document.createElement('ion-icon');
+    icon.setAttribute('name','ellipse-outline');
+    
+    const check_button = document.createElement('button');
+    check_button.appendChild(icon);
+    // Setting the id of the checkbox
+    check_button.setAttribute('id','checkbox' + i);
 
-        check_button.appendChild(icon);
+    check_button.appendChild(icon);
 
-        // Setting the value of the new task to display 
-        const task = document.createElement('p');
-        task.innerHTML = array_tasks[i];
+    // Setting the value of the new task to display 
+    const task = document.createElement('p');
+    task.innerHTML = array_tasks[i];
 
-        // Adding the elements to the content section 
-        content.appendChild(check_button);
-        content.appendChild(task);
-        new_task.appendChild(content);
+    // Adding the elements to the content section 
+    content.appendChild(check_button);
+    content.appendChild(task);
+    new_task.appendChild(content);
 
-        // aking the delete button and adding it to the task div
-        const delete_task = document.createElement('ion-icon');
-        delete_task.setAttribute('name','close-circle');
-        
-        const delete_button = document.createElement('button');
-        delete_button.appendChild(delete_task);
-        delete_button.setAttribute('id','delete' + i);
-        delete_button.setAttribute('class','delete');
+    // aking the delete button and adding it to the task div
+    const delete_task = document.createElement('ion-icon');
+    delete_task.setAttribute('name','close-circle');
+    
+    const delete_button = document.createElement('button');
+    delete_button.appendChild(delete_task);
+    delete_button.setAttribute('id','delete' + i);
+    delete_button.setAttribute('class','delete');
 
-        delete_button.appendChild(delete_task);
+    delete_button.appendChild(delete_task);
 
-        new_task.appendChild(delete_button);
+    new_task.appendChild(delete_button);
+    new_task.classList.add('animate__animated');
+    new_task.classList.add('animate__backInDown');
 
-        // Adding the task to the tasks .
-        tasks.appendChild(new_task);
-    }
+    // Adding the task to the tasks .
+    tasks.appendChild(new_task);
 }
-
-displayTasks();
 
 function displayNewTasks(){
     const new_task = document.createElement('div');
@@ -137,42 +148,51 @@ function displayNewTasks(){
     delete_button.appendChild(delete_task)
 
     new_task.appendChild(delete_button);
+    new_task.classList.add('animate__animated');
+    new_task.classList.add('animate__backInDown');
 
     tasks.appendChild(new_task);
 }
 
 // Checking the task done
-function checkedTask() {
-    // Looping thourgh each checked buttons and setting up event listeners
-    for(let i = 0;i < 2;i++){
-        let count = 0;
-        let id_string = 'checkbox' + i;
-        let button_id = document.getElementById(id_string);
-        button_id.addEventListener('click',function(){
-            if(count % 2 == 0){
-                // Changing the icon is the user clicks on the checkbox
-                button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','checkmark-done-circle-outline');
-            }else{
-                // setting the previous icon if the user clicks on the ion again.
-                button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','ellipse-outline');
-            }
-            // Toggling the class which makes the text decoration line-through whenever checkbox is checked
-            button_id.parentElement.classList.toggle('design');
-            count++;
-        });
-    }
+
+// Looping thourgh each checked buttons and setting up event listeners for checkbox
+for(let i = 0;i < 2;i++){
+    let count = 0;
+    let id_string = 'checkbox' + i;
+    let button_id = document.getElementById(id_string);
+    button_id.addEventListener('click',function(){
+        if(count % 2 == 0){
+            // Changing the icon is the user clicks on the checkbox
+            button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','checkmark-done-circle-outline');
+            // Handling Animations
+            button_id.parentElement.classList.remove('animate__animated');
+            button_id.parentElement.classList.remove('animate__headShake');
+            button_id.parentElement.classList.add('animate__animated');
+            button_id.parentElement.classList.add('animate__flash');
+        }else{
+            // setting the previous icon if the user clicks on the ion again.
+            button_id.getElementsByTagName('ion-icon')[0].setAttribute('name','ellipse-outline');
+            // Handling Animations
+            button_id.parentElement.classList.remove('animate__animated');
+            button_id.parentElement.classList.remove('animate__flash');
+            button_id.parentElement.classList.add('animate__animated');
+            button_id.parentElement.classList.add('animate__headShake');
+        }
+        // Toggling the class which makes the text decoration line-through whenever checkbox is checked
+        button_id.parentElement.classList.toggle('design');
+        count++;
+    });
 }
 
-checkedTask();
-
-function deleteTask() {
-    // Looping thourgh each checked buttons and setting up event listeners
-    for(let i = 0;i < array_tasks.length;i++){
-        let id_string = 'delete' + i;
-        let button_id = document.getElementById(id_string);
-        button_id.addEventListener('click',function(){
+// Looping thourgh each checked buttons and setting up event listeners for delete task
+for(let i = 0;i < array_tasks.length;i++){
+    let id_string = 'delete' + i;
+    let button_id = document.getElementById(id_string);
+    button_id.addEventListener('click',function(){
+        button_id.parentElement.classList.add('animate__backOutDown');
+        setTimeout(() => {
             button_id.parentElement.remove();
-        });
-    }
+        }, 500);
+    });
 }
-deleteTask()
